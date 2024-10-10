8eda6373421749af561f10a27a133148d3d3a6de
jiangwei <1048851186@qq.com>
2024-10-10 15:45:00 +0800

    Initial Commit

---
+++ ColorAlphaUITests/ColorAlphaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ColorAlphaUITestsLaunchTests.swift
+//  ColorAlphaUITests
+//
+//  Created by edy on 2024/10/10.
+//
+
+import XCTest
+
+final class ColorAlphaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
